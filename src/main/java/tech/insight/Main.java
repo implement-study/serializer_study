46262bd684733903f8eee4eed6f8467c4b815baa
gongxuanzhang <gongxuanzhangmelt@gmail.com>
2025-12-04 21:47:56 +0800

    first commit

--- src/main/java/tech/insight/Main.java
+++ src/main/java/tech/insight/Main.java
@@ -1,8 +1,44 @@
 package tech.insight;
 
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {
+        User user = new User();
+        user.setName("张三");
+        user.setAge(18);
+
+        byte[] userBytes = serializeUser(user);
+        File file = new File("user.ss");
+        Files.write(file.toPath(), userBytes);
+        System.out.println(file.length());
+
+        byte[] readAllBytes = Files.readAllBytes(file.toPath());
+        User deUser = deserialize(readAllBytes);
+        System.out.println(deUser);
     }
+
+    private static byte[] serializeUser(User user) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(byteArrayOutputStream)) {
+            outputStream.writeObject(user);
+        }
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    private static User deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream arrayInputStream = new ByteArrayInputStream(bytes);
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(arrayInputStream)) {
+            return (User) objectInputStream.readObject();
+        }
+
+    }
+
 }
